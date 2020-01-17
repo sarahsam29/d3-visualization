b2fdd6ade06b1b7dc67df5cfc98966e7eb8275be
sarahsam29 <47089625+sarahsam29@users.noreply.github.com>
2020-01-16 23:44:11 -0500

    Add files via upload

--- scales-cars.js
+++ scales-cars.js
@@ -1,52 +1,62 @@
-//console.log(Math.max(cars.map(d=>d.mpg)))
-//console.log(d3.min(cars.map(d=>d.mpg)))
-//console.log(d3.max(cars.map(d=>d.mpg)))
+let margin = {top: 30, right: 30, bottom: 50, left: 50}
+let width = 700,
+    height = 500;
+let innerWidth = width - margin.left - margin.right,
+    innerHeight = height - margin.top - margin.bottom;
 
-let margin = {top: 30, right: 30, bottom: 30, left: 30}
-let width = 600, height = 400;
+let background = d3.select('body')
+                  .select('svg')
+                  .attr('id', 'scatterPlot')
+                  .attr('width', width)
+                  .attr('height', height)
+                  .append('g')
+                  .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 
-d3.select('body')
-  .select('svg')
-  .attr('id', 'scatterPlot')
-  .attr('width', width)
-  .attr('height', height)
   //.style('background-color', '#FCF3CF')
 
 //x scale
   let dispScale = d3.scaleLinear()
         .domain(d3.extent(cars.map(d=> d.disp)))
-        .range([50,600])
+        .range([20, innerWidth - 20])
 
 //y scale
   let mpgScale = d3.scaleLinear()
         .domain(d3.extent(cars.map(d=> d.mpg)))
-        .range([400,50])
+        .range([20, innerHeight - 20].reverse())
+        //.range([300,20])
 
 //size scale
   let wtScale = d3.scaleSqrt()
       .domain(d3.extent(cars.map(d=> d.wt)))
-      .range([3,30])
+      .range([3,25])
 
 //colour scale
   let cylScale = d3.scaleQuantile()
     .domain(d3.extent(cars.map(d=>d.cyl)))
     .range(['#95b7ec', '#ffe787','#eabcbc', '#90c9c5'])
 
-d3.selectAll('svg#scatterPlot')
-  .selectAll('circle')
-  .data(cars)
-  .enter()
-  .append('circle')
-  .attr('cx',d => dispScale(d.disp))
-  .attr('cy',d => mpgScale(d.mpg))
-  .attr('r', d => wtScale(d.wt))
-  .attr('fill',d => cylScale(d.cyl))
-  .style('opacity', 0.8)
+let circles = background.selectAll('circle')
+                .data(cars)
+                .enter()
+                .append('circle')
+                .attr('cx',d => dispScale(d.disp))
+                .attr('cy',d => mpgScale(d.mpg))
+                .attr('r', d => wtScale(d.wt))
+                .attr('fill',d => cylScale(d.cyl))
+                .style('opacity', 0.8)
+                //.transition()
+                //.duration(2000)
 
-let xAxis = d3.axisBottom(dispScale)
-let yAxis = d3.axisLeft(mpgScale)
+// axes
+let xAxis = d3.axisBottom()
+              .scale(dispScale)
+let yAxis = d3.axisLeft()
+              .scale(mpgScale)
 
-d3.selectAll('svg#scatterPlot')
-  .call(xAxis)
-  .attr()
-  //.call(yAxis)
+background.append('g')
+          .attr("transform", "translate(-18, 410)")
+          .call(xAxis);
+
+background.append('g')
+          .attr("transform", "translate(0,10)")
+          .call(yAxis);
